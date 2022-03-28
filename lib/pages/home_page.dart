230feca834037c44d32fa76d6f13478713d48024
arjun-baidya <arjun.chandra.baidya@gmail.com>
2022-03-28 17:31:09 +0600

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_learning/models/catalog.dart';
 import 'package:flutter_learning/widgets/drawer.dart';
 
 import '../widgets/drawer.dart';
+// import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,12 +14,12 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('OUM'),
       ),
-      body: ListView.builder(
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index){
-          return ItemWidget();
-        },
-      ),
+      // body: ListView.builder(
+      //   itemCount: CatalogModel.items.length,
+      //   itemBuilder: (context, index){
+          
+      //   },
+      // ),
       drawer: const MyDrawer(),
     );
   }
